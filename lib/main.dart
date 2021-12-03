0e6a09d134665b78a052da36abe0a27b812ed645
KheavSunny <kheavsunny0521@gmail.com>
2021-12-03 16:46:50 +0700

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sidebar/NavBar.dart';
+import 'package:sidebar/navbar.dart';
 void main() {
   runApp(const MyApp());
 }
--- lib/navbar.dart
+++ lib/navbar.dart
@@ -2,9 +2,10 @@
 import 'package:flutter/material.dart';
 
 class NavBar extends StatelessWidget{
+  const NavBar({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Drawer(
+    return const Drawer(
 
     );
   }
