63efa5073adc066b8465444000cab718db74c3cc
KheavSunny <kheavsunny0521@gmail.com>
2021-12-03 17:01:11 +0700

    Fourth Commit

--- lib/navbar.dart
+++ lib/navbar.dart
@@ -7,6 +7,7 @@ class NavBar extends StatelessWidget{
   Widget build(BuildContext context) {
     return Drawer(
         child: ListView(
+          padding: EdgeInsets.zero,
           children: <Widget>[
             UserAccountsDrawerHeader(
                 accountName: Text('KheavSunny'),
@@ -14,11 +15,21 @@ class NavBar extends StatelessWidget{
                 currentAccountPicture: CircleAvatar(
                   child: ClipOval(
                     child: Image.network(
-                        'https://www.clipartmax.com/png/middle/171-1717870_stockvader-predicted-cron-for-may-user-profile-icon-png.png'
+                        'https://www.clipartmax.com/png/middle/171-1717870_stockvader-predicted-cron-for-may-user-profile-icon-png.png',
+                      fit: BoxFit.cover,
+                      width: 200,
+                      height: 200,
                     ),
                   ),
                 ),
-            )
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage('https://i.pinimg.com/originals/8a/36/f1/8a36f1b33b436275a8424ab0d7c307da.jpg'),
+                  fit: BoxFit.cover,
+
+                ),
+              ),
+            ),
           ],
         ),
     );
